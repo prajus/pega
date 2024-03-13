8be7217708359b9fa79612bf6d3beb76f08a2d55
Administrator <Administrator@172.31.6.214>
2024-03-13 07:27:36 +0000

    commit with TestNG

---
+++ seleniumtest1/src/main/java/selenium/Chkbox2.java
@@ -0,0 +1,52 @@
+package selenium;
+
+import java.util.List;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+public class Chkbox2 {
+	WebDriver driver;
+	WebElement chkbox;
+	
+	@BeforeClass
+	public void setup() {
+		driver = new ChromeDriver();
+		driver.manage().window().maximize();
+	}
+	
+@Test
+//Run | Debug
+public void testcase1() {
+	driver.get("https://demo.seleniumeasy.com/basic-checkbox-demo.html");
+	String title = driver.getTitle();
+	String exptitle = "Selenium Easy - Checkbox demo for automation using selenium";
+	Assert.assertEquals(title, exptitle);
+}
+@Test
+public void testcase2() {
+	chkbox = driver.findElement(By.cssSelector("#isAgeSelected"));
+	chkbox.click();
+	System.out.println("After Select: "+chkbox.isSelected());
+	Assert.assertTrue(chkbox.isSelected());
+}
+@Test
+public void testcase3() {
+	
+	chkbox.click();
+	System.out.println("After UnSelect: "+chkbox.isSelected());
+	Assert.assertTrue(chkbox.isSelected());
+}
+
+@AfterClass
+public void teardown() {
+	driver.quit();
+}
+}
---
+++ seleniumtest1/src/main/java/selenium/TestNGSample.java
@@ -0,0 +1,10 @@
+package selenium;
+
+import org.testng.annotations.Test;
+
+public class TestNGSample {
+	@Test
+	public void testcase(){
+		System.out.println("testcase printed");
+	}
+}
